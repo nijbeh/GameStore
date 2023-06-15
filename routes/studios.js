cb2b390ac760cee7c3aa8f0ad2b2531f4a271ed4
Nicoman <nbehbudov@ea.com>
2023-06-14 21:15:07 -0700

    showoao fsf

--- routes/studios.js
+++ routes/studios.js
@@ -11,15 +11,24 @@ router.get('/new', (req,res) => {
     res.send ('New studio addition Form')
 })
 
+
+
 router.route('/:studioID')
 .get((req,res) => {
-    res.send (`Get specific Studio with ${req.params.studioID}`)
+    res.send (req.user)
 })
 .put((req,res) => {
     res.send (`Put specific Studio with ${req.params.studioID}`)
 })
 .delete ((req,res) =>{
     res.send (`Delete specific Studio with ${req.params.studioID}`)
+})
+
+const studio_names = [{name: "Electronic Arts"}, {name : "Activision"}]
+
+router.param("studioID", (req,res,next,studio_id) =>{
+    req.user = studio_names[studio_id]
+    next()
 })
 
 module.exports = router
